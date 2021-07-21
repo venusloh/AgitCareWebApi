410e7eb87cbd4b4e7f55af5487f39bcd0dd810fb
venusloh <61502873+venusloh@users.noreply.github.com>
2021-07-21 22:13:07 +0800

    EditedForHeroku

--- index.js
+++ index.js
@@ -15,5 +15,6 @@ app.use('/api', userRoutes.routes);
 
 
 
-app.listen(config.port, () => console.log('App is listening on url http://localhost:' + config.port));
-
+app.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
+     });
