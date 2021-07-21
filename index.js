f81a092b830858542601f3ef58f63025b85fdf72
venusloh <61502873+venusloh@users.noreply.github.com>
2021-07-22 00:50:18 +0800

    Heroku

--- index.js
+++ index.js
@@ -4,8 +4,11 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const config = require('./config');
 const userRoutes = require('./routes/user-routes');
+const path = require('path');
 
 const app = express();
+const port = process.env.PORT || 8080;
+
 
 app.use(express.json());
 app.use(cors());
@@ -13,8 +16,8 @@ app.use(bodyParser.json());
 
 app.use('/api', userRoutes.routes);
 
+app.use(express.static(__dirname + '/public'));
+  
 
-
-app.listen(app.get('port'), function() {
-    console.log('Node app is running on port', app.get('port'));
-     });
+  app.listen(port);
+  console.log('Server started at http://localhost:' + port);
